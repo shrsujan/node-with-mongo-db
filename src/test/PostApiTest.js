0450de43d940a2c8d014f9bd40864ba216d84289
sujan <shrsujan2007@gmail.com>
2017-04-18 23:01:53 +0545

    job post remaining api tests

--- src/test/PostApiTest.js
+++ src/test/PostApiTest.js
@@ -196,5 +196,122 @@ describe('Post Api test', function () {
         done()
       })
     })
+
+    it('retrieves existing post from database', function (done) {
+      supertest(app)
+      .get('/job/' + testLocation.country + '/' + testLocation.state + '/' + testLocation.city + '/' + testPost.category + '/' + testPost.subCategory + '/' + testPost.postId)
+      .set('authorization', token)
+      .expect(200)
+      .end(function (err, response) {
+        if (err) {
+          done(err)
+        }
+        let res = JSON.parse(JSON.parse(JSON.stringify(response)).text)
+        expect(res).to.be.an('object')
+        expect(res).to.have.property('success', 1)
+        expect(res).to.have.property('message', 'Post retrieved successfully')
+        expect(res).to.have.property('data').that.is.an('object')
+        expect(res.data).to.have.property('title', testPost.title)
+        expect(res.data).to.have.property('content', testPost.content)
+        expect(res.data).to.have.property('salary', testPost.salary)
+        expect(res.data).to.have.property('titleImage').that.is.a('string')
+        expect(res.data).to.have.property('category', testPost.category.toLowerCase())
+        expect(res.data).to.have.property('subCategory', testPost.subCategory.toLowerCase())
+        expect(res.data).to.have.property('postId', testPost.postId.toLowerCase())
+        expect(res.data).to.have.property('locationDetails').that.is.an('object')
+        expect(res.data.locationDetails).to.have.property('country', testLocation.country.toLowerCase())
+        expect(res.data.locationDetails).to.have.property('zip', testLocation.zip)
+        expect(res.data.locationDetails).to.have.property('state', testLocation.state.toLowerCase())
+        expect(res.data.locationDetails).to.have.property('nearestBiggestCity', testLocation.nearestBiggestCity.toLowerCase())
+        expect(res.data.locationDetails).to.have.property('city', testLocation.city.toLowerCase())
+        expect(res.data.locationDetails).to.have.property('lat', testLocation.lat)
+        expect(res.data.locationDetails).to.have.property('long', testLocation.long)
+        expect(res.data.locationDetails).to.have.property('streetAddress', testLocation.streetAddress.toLowerCase())
+        expect(res.data.locationDetails).to.have.property('placeId', testLocation.placeId.toLowerCase())
+        expect(res.data).to.have.property('userDetails').that.is.an('object')
+        expect(res.data.userDetails).to.have.property('firstName', testUser.firstName)
+        expect(res.data.userDetails).to.have.property('lastName', testUser.lastName)
+        expect(res.data.userDetails).to.have.property('email', testUser.email)
+        expect(res.data.userDetails).to.not.have.property('password')
+        expect(res.data.userDetails).to.have.property('deviceId', testUser.deviceId)
+        expect(res.data.userDetails).to.have.property('profilePic').that.is.a('string')
+        done()
+      })
+    })
+
+    it('updates existing post', function (done) {
+      let newTestPost = {
+        title: 'newTitle',
+        content: 'new content',
+        salary: 999999
+      }
+
+      supertest(app)
+      .put('/job/' + testLocation.country + '/' + testLocation.state + '/' + testLocation.city + '/' + testPost.category + '/' + testPost.subCategory + '/' + testPost.postId)
+      .set('authorization', token)
+      .field('title', newTestPost.title)
+      .field('content', newTestPost.content)
+      .field('salary', newTestPost.salary)
+      .attach('titleImage', 'src/test/fixtures/image.jpg')
+      .expect(200)
+      .end(function (err, response) {
+        if (err) {
+          done(err)
+        }
+        let res = JSON.parse(JSON.parse(JSON.stringify(response)).text)
+        expect(res).to.be.an('object')
+        expect(res).to.have.property('success', 1)
+        expect(res).to.have.property('message', 'Post updated successfully')
+        expect(res).to.have.property('data').that.is.an('array')
+        expect(res.data[0]).to.have.property('title', newTestPost.title)
+        expect(res.data[0]).to.have.property('content', newTestPost.content)
+        expect(res.data[0]).to.have.property('salary', newTestPost.salary)
+        expect(res.data[0]).to.have.property('titleImage').that.is.a('string')
+        expect(res.data[0]).to.have.property('category', testPost.category.toLowerCase())
+        expect(res.data[0]).to.have.property('subCategory', testPost.subCategory.toLowerCase())
+        expect(res.data[0]).to.have.property('postId', testPost.postId.toLowerCase())
+        expect(res.data[0]).to.have.property('locationDetails').that.is.an('object')
+        expect(res.data[0].locationDetails).to.have.property('country', testLocation.country.toLowerCase())
+        expect(res.data[0].locationDetails).to.have.property('zip', testLocation.zip)
+        expect(res.data[0].locationDetails).to.have.property('state', testLocation.state.toLowerCase())
+        expect(res.data[0].locationDetails).to.have.property('nearestBiggestCity', testLocation.nearestBiggestCity.toLowerCase())
+        expect(res.data[0].locationDetails).to.have.property('city', testLocation.city.toLowerCase())
+        expect(res.data[0].locationDetails).to.have.property('lat', testLocation.lat)
+        expect(res.data[0].locationDetails).to.have.property('long', testLocation.long)
+        expect(res.data[0].locationDetails).to.have.property('streetAddress', testLocation.streetAddress.toLowerCase())
+        expect(res.data[0].locationDetails).to.have.property('placeId', testLocation.placeId.toLowerCase())
+        expect(res.data[0]).to.have.property('userDetails').that.is.an('object')
+        expect(res.data[0].userDetails).to.have.property('firstName', testUser.firstName)
+        expect(res.data[0].userDetails).to.have.property('lastName', testUser.lastName)
+        expect(res.data[0].userDetails).to.have.property('email', testUser.email)
+        expect(res.data[0].userDetails).to.not.have.property('password')
+        expect(res.data[0].userDetails).to.have.property('deviceId', testUser.deviceId)
+        expect(res.data[0].userDetails).to.have.property('profilePic').that.is.a('string')
+        fs.stat('src/public/images/posts/' + res.data[0].titleImage, function (e, stats) {
+          if (e) done(e)
+          fs.unlink('src/public/images/posts/' + res.data[0].titleImage, function (err) {
+            if (err) done(err)
+          })
+        })
+        done()
+      })
+    })
+
+    it('deletes existing post', function (done) {
+      supertest(app)
+      .delete('/job/' + testLocation.country + '/' + testLocation.state + '/' + testLocation.city + '/' + testPost.category + '/' + testPost.subCategory + '/' + testPost.postId)
+      .set('authorization', token)
+      .expect(200)
+      .end(function (err, response) {
+        if (err) {
+          done(err)
+        }
+        let res = JSON.parse(JSON.parse(JSON.stringify(response)).text)
+        expect(res).to.be.an('object')
+        expect(res).to.have.property('success', 1)
+        expect(res).to.have.property('message', 'Post deleted successfully')
+        done()
+      })
+    })
   })
 })
